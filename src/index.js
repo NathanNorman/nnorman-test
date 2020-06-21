70821a44ae47814730b97213d5054561b8446a8c
matteo8p <matt8p@outlook.com>
2020-06-21 15:55:28 -0400

    m

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const github = require('@actions/github');
 const path = require('path');
 const fs = require('fs');
 
-const srcPath = core.getInput("src_dir")
-core.info("Reading " + srcPath);
+const srcPath = core.getInput("sql_directory")
+core.info("Reading Path: " + srcPath);
 
-fs.readdir(srcPath, function (err, files) {
+fs.readdir(srcPath, function (err, files)
+{
   //handling error
   if (err) {
     return console.log('Unable to scan directory: ' + err);
