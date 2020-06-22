8382e8aec86ddd06587d85aedac994832a4f2eb1
matteo8p <matt8p@outlook.com>
2020-06-22 16:25:40 -0400

    master

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 
-const srcPath = core.getInput("sql_directory")
+const srcPath = core.getInput("sql_directory");
+const master = core.getInput("master_sql");
 
 fs.readdir(srcPath, function (err, files)
 {
   core.info("Displaying files: ");
   core.info(files);
 });
-
+core.info(master);
 core.info('Finished Running');
