b2e5971c6913f7f8a3687fd4b9310a1e4d05fe94
matteo8p <matt8p@outlook.com>
2020-06-22 17:25:47 -0400

    m

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 
-const masterSQL = core.getInput("master_sql");
-const currentSQL = core.getInput("current_sql");
+const MASTERSQL = core.getInput("master_sql");
+const CURRENTSQL = core.getInput("current_sql");
 
-console.log("MASTER SQL:");
-console.log(masterSQL);
-console.log("CURRENT SQL: ");
-console.log(currentSQL);
+var master_list = MASTERSQL.split(' ');
+var current_list = CURRENTSQL.split(' ');
+
+for(var i = 0; i < master_list.length; i++)
+{
+  console.log(master_list[i]);
+}
+
+for(var i = 0; i < current_list.length; i++)
+{
+  console.log(current_list[i]);
+}
