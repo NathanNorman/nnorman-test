0d9347d0728c095802496aa339707c2a8792c3fe
matteo8p <matt8p@outlook.com>
2020-06-22 17:41:50 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -2,18 +2,50 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 
-const MASTERSQL = core.getInput("master_sql");
-const CURRENTSQL = core.getInput("current_sql");
+//  INITIATE VARIABLES
+const MASTERSQL = core.getInput("master_sql");        //Pulls String of sql files from master
+const CURRENTSQL = core.getInput("current_sql");      //Pulls String of sql files from current branch
 
 var master_list = MASTERSQL.split(' ');
 var current_list = CURRENTSQL.split(' ');
+//  END INITIATE VARIABLES
 
-for(var i = 0; i < master_list.length; i++)
+init();     //call initialize method
+
+//  INITIALIZE METHOD
+function init()
 {
-  console.log(master_list[i]);
+  var newSQLFiles = newSQLFiles();
+
+  if(newSQLFiles.length == 0)
+  {
+    TERMINATE_SUCCESS("No changes made to sql files");
+  }
 }
 
-for(var i = 0; i < current_list.length; i++)
+function newSQLFiles()
 {
-  console.log(current_list[i]);
+  var newSQL = [];
+  for(var i = 0; i < current_list.length; i++)
+  {
+    if(!master_list.includes(current_list[i]))
+    {
+      newSQL.push(current_list[i]);
+    }
+  }
+  return newSQL;
 }
+
+
+
+//  TERMINATION METHODS
+function TERMINATE_FAIL(message)
+{
+  core.error("ERROR: " + message);
+}
+
+function TERMINATE_SUCCESS(message)
+{
+  core.info("SUCCESS: " + message);
+}
+//  END TERMINATION METHODS
