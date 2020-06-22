8bdd7a172ed02fedc2826c890eee0655a0c24099
matteo8p <matt8p@outlook.com>
2020-06-22 13:17:46 -0400

    rundir

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 const srcPath = core.getInput("sql_directory")
-var sqlDirectories;
 
 fs.readdir(srcPath, function (err, files)
 {
   console.log(files);
-  for(var i = 0; i < files.length; i++)
-  {
-    console.log(files[i]);
-  }
 });
 
 core.info('Finished Running');
