5b5b047925e0727380e62cbc69676d88b3f68770
matteo8p <matt8p@outlook.com>
2020-06-22 11:17:41 -0400

    payload

--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ const fs = require('fs');
 const srcPath = core.getInput("sql_directory")
 core.info("Reading Path: " + srcPath);
 
-fs.readdir(srcPath, function (err, files)
-{
-  //handling error
-  if (err) {
-    return console.log('Unable to scan directory: ' + err);
-  }
-  //listing all files using forEach
-  files.forEach(function (file)
-  {
-    core.info(file.valueOf());
-  });
-});
+const payload = github.context.payload;
+core.info(payload);
+
+// fs.readdir(srcPath, function (err, files)
+// {
+//   //handling error
+//   if (err) {
+//     return console.log('Unable to scan directory: ' + err);
+//   }
+//   //listing all files using forEach
+//   files.forEach(function (file)
+//   {
+//     core.info(file.valueOf());
+//   });
+// });
 
 core.info('Finished Running');
