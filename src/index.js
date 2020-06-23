5126a6c08882490a4ec7c31d456ea74a1fa1e408
matteo8p <matt8p@outlook.com>
2020-06-23 11:55:05 -0400

    regex

--- src/index.js
+++ src/index.js
@@ -9,50 +9,62 @@ const CURRENTSQL = core.getInput("current_sql");      //Pulls String of sql file
 const master_list = MASTERSQL.split(' ');
 const current_list = CURRENTSQL.split(' ');
 
+
 // INPUT: NONE
 // OUTPUT: Array of new sql files
 // Compares current_list to master_list to generate array of new sql files
 function newSQLFiles()
 {
   var newSQL = [];
-  for(var i = 0; i < current_list.length; i++)
+  for(var file in current_list)
   {
-    if(!master_list.includes(current_list[i]))
+    if(!master_list.includes(file))
     {
       newSQL.push(current_list[i]);
     }
   }
+  core.info("New files detected: " + newSQL.toString());
   return newSQL;
 }
 
+function checkFileFormat(newSQL)
+{
+  core.info("Initiate Regex File Format Test");
+  const regex = RegExp("v" + year + ".[0-1][1-9].[0-3][0-9]_\\d{2}__.*");
+  for(file in newSQL)
+  {
+    core.info("Checking " + file);
+    if(!regex.test(file)) TERMINATE_FAIL(file + " fails to match format. Format must be in format vYYYY.MM.DD.xx__Description");
+  }
+  core.info("Regex File Format Test Successful!");
+}
 
 
 //  TERMINATION METHODS
 function TERMINATE_FAIL(message)
 {
-  core.error("ERROR: " + message);
-  process.exit(1);
+  core.setFailed("FAILED: " + message);
 }
 
 function TERMINATE_SUCCESS(message)
 {
   core.info("SUCCESS: " + message);
-  process.exit(1);
 }
-//  END TERMINATION METHODS
 
+//  INITIATION METHODS
+function runTests(newSQL)                               //Runs tests in sequence
+{
+  checkFileFormat(newSQL);
+  TERMINATE_SUCCESS(" All tests have passed");
+}
 
-
-
-//  INITIALIZE METHOD
 function init()
 {
-  var newSQL = newSQLFiles();
+  var newSQL = newSQLFiles();                           //Array of new sql files
 
-  if(newSQL.length == 0) TERMINATE_SUCCESS("No changes made to sql files"); //No new sql files added. Terminate check as successful
-
-  console.log(newSQL);
-  TERMINATE_SUCCESS("Done");
+  if(newSQL.length == 0)
+    TERMINATE_SUCCESS("No changes made to sql files");  //No new sql files added. Terminate check as successful
+  else
+    runTests(newSQL);                                   //If there are sql files added, run the other tests
 }
-
-init();     //call initialize method
+init();                                                 //call initialize method
