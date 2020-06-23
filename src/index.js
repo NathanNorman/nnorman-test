42b75522301d2d8823c7f73205e45c6763c18869
matteo8p <matt8p@outlook.com>
2020-06-23 12:51:04 -0400

    year

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 //  INSTANCE VARIABLES
 const MASTERSQL = core.getInput("master_sql");        //Pulls String of sql files from master
 const CURRENTSQL = core.getInput("current_sql");      //Pulls String of sql files from current branch
+const YEAR = core.getInput("year");
 
 const master_list = MASTERSQL.split(' ');          //Divide MASTERSQL into Array
 const current_list = CURRENTSQL.split(' ');        //Divide CURRENTSQL into Array
@@ -34,26 +35,30 @@ function fileFormatTest(newSQL)
 {
   NEW_SECTION("Initiate Regex File Format Test");
 
-  const regex = RegExp("v" + "2020" + ".[0-1][1-9].[0-3][0-9]_\\d{2}__.*");
+  const regex = RegExp("v" + YEAR + ".[0-1][1-9].[0-3][0-9]_\\d{2}__.*");
   for(var i = 0; i < newSQL.length; i++)
   {
     core.info("Checking " + newSQL[i]);
-    if(!regex.test(newSQL[i]))                        //Fail fileFormatTest
+    if(!regex.test(newSQL[i]))
+    {
       TERMINATE_FAIL(newSQL[i] + " fails to match format. Format must be in format vYYYY.MM.DD.xx__Description");
+    }
   }
   core.info("Regex File Format Test Successful!");
 }
 
 
-//  LOGICMETHODS
+//  PROGRAM FLOW METHODS
 function TERMINATE_FAIL(message)
 {
   core.setFailed("FAILED: " + message);
+  process.exit(1);
 }
 
 function TERMINATE_SUCCESS(message)
 {
-  core.info("SUCCESS: " + message);
+  core.info("TESTS SUCCESS: " + message);
+  process.exit(0);
 }
 
 function NEW_SECTION(message)
@@ -63,23 +68,22 @@ function NEW_SECTION(message)
   core.info("-----------------------------------");
 }
 
+
 //  INITIATION METHODS
 function runTests(newSQL)                               //Runs tests in sequence
 {
-  fileFormatTest(newSQL);
-  TERMINATE_SUCCESS(" All tests have passed");          //When all tests have passed.
+  fileFormatTest(newSQL);                               //Run file format test
+  TERMINATE_SUCCESS("Have a good rest of your day!");   //When all tests have passed.
 }
 
 function init()
 {
   var newSQL = newSQLFiles();                           //Array of new sql files
 
+  NEW_SECTION("Scanning for new sql files");
   if(newSQL.length == 0)
-  {
     TERMINATE_SUCCESS("No changes made to sql files");  //No new sql files added. Terminate check as successful
-  } else
-  {
-    runTests(newSQL);                                   //If there are sql files added, run the other tests
-  }
+
+  runTests(newSQL);                                     //If there are sql files added, run the other tests
 }
 init();                                                 //call initialize method
